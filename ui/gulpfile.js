d2800342fc6cb8efcc1675ae03250f332dca2c8e
Alastair Brayne <al@perchten.co.uk>
2014-06-20 16:04:24 +0100

    Added some gulp modules

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,4 +1,21 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+
+
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+
+gulp.task('vendor', function() {
+    return gulp.src('vendor/*.js')
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('build/vendor.js'))
+});
+
 
 gulp.task('default', function() {
     // place code for your default task here
